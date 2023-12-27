420937d2326bbbf72e635915be36569771b7d4bd
SteveLorde <mostafa.maher98@gmail.com>
2023-12-27 20:20:23 +0200

    AutoMapper, Startup, DataContext services

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -5,4 +5,20 @@ namespace MyAPI.Data;
 public class DataContext : DbContext
 {
     
+    private readonly IConfiguration _configenv;
+
+    public DataContext(IConfiguration configenv)
+    {
+        _configenv = configenv;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(_configenv["DatabaseConnection"]);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+
 }
--- Services/Startup/Startup.cs
+++ Services/Startup/Startup.cs
@@ -1,6 +1,26 @@
-﻿namespace MyAPI.Services.Startup;
+﻿using Microsoft.EntityFrameworkCore;
+using MyAPI.Data;
+
+namespace MyAPI.Services.Startup;
 
 public class Startup
 {
-    
+    private readonly IServiceProvider _serviceprovider;
+    private readonly IWebHostEnvironment _webenv;
+
+    public Startup(IServiceProvider serviceProvider, IWebHostEnvironment webenv)
+    {
+        _serviceprovider = serviceProvider;
+        _webenv = webenv;
+    }
+
+    public void ExecuteServices()
+    {
+        var storagefolder = Path.Combine(_webenv.ContentRootPath, "Storage");
+        Directory.CreateDirectory(storagefolder);
+        var scopedb = _serviceprovider.CreateScope();
+        var servicescopedb = scopedb.ServiceProvider;
+        var dbservice = servicescopedb.GetRequiredService<DataContext>();
+        dbservice.Database.Migrate();
+    }
 }
