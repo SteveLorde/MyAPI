1d0d6dff6a2e4b54168cae7ad1547e2d6beb568d
SteveLorde <mostafa.maher98@gmail.com>
2023-12-27 20:43:08 +0200

    register startup service and controllers in program startup class

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,13 +6,16 @@ public class DataContext : DbContext
 {
     
     private readonly IConfiguration _configenv;
+    private readonly IWebHostEnvironment _hostenv;
 
-    public DataContext(IConfiguration configenv)
+    public DataContext(IConfiguration configenv, IWebHostEnvironment hostenv)
     {
         _configenv = configenv;
+        _hostenv = hostenv;
     }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+
         optionsBuilder.UseSqlite(_configenv["DatabaseConnection"]);
     }
 
--- Program.cs
+++ Program.cs
@@ -1,16 +1,31 @@
 using System.Text.Json.Serialization;
+using Microsoft.Extensions.FileProviders;
 using MyAPI.Data;
 using MyAPI.Services.AutoMapper;
+using MyAPI.Services.Startup;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddDbContext<DataContext>();
 builder.Services.AddAutoMapper(typeof(AutoProfile));
+builder.Services.AddScoped<Startup>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: "CorsPolicy", builder =>
+    {
+        builder.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+    });
+});
 
 var app = builder.Build();
+
+var servicescope = app.Services.CreateScope();
+var services = servicescope.ServiceProvider;
+var startupservice = services.GetRequiredService<Startup>();
+startupservice.ExecuteServices();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,5 +34,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "Storage")),
+    RequestPath = "/storage"
+});
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
